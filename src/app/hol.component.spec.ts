52366d6749f5e5ceb5ebfaa463a553629263bf0f
christopher-harris <ccharris0607@outlook.com>
2020-11-05 17:01:51 -0600

    HOL Tests

--- src/app/hol.component.spec.ts
+++ src/app/hol.component.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './hol.component';
+import { HolComponent } from './hol.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -9,27 +9,14 @@ describe('AppComponent', () => {
         RouterTestingModule
       ],
       declarations: [
-        AppComponent
+        HolComponent
       ],
     }).compileComponents();
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const fixture = TestBed.createComponent(HolComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'hands-on-labs'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('hands-on-labs');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('hands-on-labs app is running!');
   });
 });
